b9b22c81c840a694ea054e35522b730be0cef3b8
SashaLinux <nikonorovsasha1@gmail.com>
2025-12-14 13:45:48 +0300

    Сделал UI до непосредственной передачи файла

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <asio.hpp>
+#include <fstream>
+#include <filesystem>
 
 using namespace std;
+using namespace filesystem;
 using namespace asio;
 using namespace asio::ip;
 
@@ -14,13 +17,39 @@ int main(){
 
 
 	while (!good){
-		cout << "Принимаю [1]\nПодключаюсь [2]\nВыбор: ";
+		cout << "Получаю файлы [1]\nОтправляю файлы [2]\nВыбор: ";
 		getline(cin, choise);
 		if (choise == "1"){
 			tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 45556));
 			tcp::socket user2 = acceptor.accept();
 
 			cout << "Cl" << endl;
+
+			bool true_path = false;
+
+			while(!true_path){
+				cout << "Введите путь к папке для копирования: ";
+				string path_, repath;
+				getline(cin, path_);
+				for (int c = 0; c < path_.length(); ++c) {
+					if (path_[c] != '"') repath += path_[c];
+				}
+				path_ = repath;
+
+				if (exists(path_)){
+					if (!is_regular_file(path_)){
+						//
+						true_path = true;
+					}
+					else{
+						cout << "Требуеться ввести путь к папке!\n\n";
+					}
+				}
+				else{
+					cout << "Такого пути не существует!\n\n";
+				}
+			}
+
 
 			good = true;
 		}
@@ -33,6 +62,31 @@ int main(){
 			user1.connect(tcp::endpoint(make_address(serv_ip), 45556));
 
 			cout << "Serv" << endl;
+
+			bool true_path = false;
+
+			while(!true_path){
+				cout << "Введите путь к файлу для копирования: ";
+				string path_, repath;
+				getline(cin, path_);
+				for (int c = 0; c < path_.length(); ++c) {
+					if (path_[c] != '"') repath += path_[c];
+				}
+				path_ = repath;
+
+				if (exists(path_)){
+					if (is_regular_file(path_)){
+						//
+						true_path = true;
+					}
+					else{
+						cout << "Требуеться ввести путь к файлу!\n\n";
+					}
+				}
+				else{
+					cout << "Такого пути не существует!\n\n";
+				}
+			}
 
 			good = true;
 		}
