b8f0c97ae65acf9cc18569f6860d23d58a3f52b3
BlackLinux <nikonorovsasha1@gmail.com[C>
2025-12-21 15:18:44 +0300

    Отправка заголовка файла - успешная

--- main.cpp
+++ main.cpp
@@ -3,11 +3,17 @@
 #include <asio.hpp>
 #include <fstream>
 #include <filesystem>
+#include <cstring>
 
 using namespace std;
 using namespace filesystem;
 using namespace asio;
 using namespace asio::ip;
+
+struct FileHeader{
+	size_t fsize;
+	size_t nsize;
+};
 
 int main(){
 	string choise;
@@ -39,6 +45,27 @@ int main(){
 				if (exists(path_)){
 					if (!is_regular_file(path_)){
 						//
+
+						unsigned char buff[1024];
+
+						size_t s = read(user2, asio::buffer(&buff, sizeof(FileHeader)));
+
+						FileHeader fh;
+						memcpy(&fh.fsize, buff, sizeof(size_t));
+						memcpy(&fh.nsize, buff+8, sizeof(size_t));
+
+						cout << endl << fh.fsize << endl << fh.nsize << endl;
+
+						read(user2, asio::buffer(buff, sizeof(buff)));
+
+						string namefile = "";
+
+						for (int i = 0; i < fh.nsize; ++i){
+							namefile += buff[i];
+						}
+
+						cout << namefile << endl;
+
 						true_path = true;
 					}
 					else{
@@ -77,6 +104,35 @@ int main(){
 				if (exists(path_)){
 					if (is_regular_file(path_)){
 						//
+
+						//Отправка заголовка
+						unsigned char buff[1024];
+						FileHeader fh;
+						fh.fsize = file_size(path_);
+						path p(path_);
+						fh.nsize = p.filename().string().size();
+						memcpy(buff, &fh, sizeof(FileHeader));
+
+						write(user1, asio::buffer(&buff, sizeof(buff)));
+
+						//Отправка имени
+						unsigned char name[1024];
+						string n = p.filename().string();
+						write(user1, asio::buffer(n, n.size()));
+
+						//Отправка файла
+						char chunk[1024];
+						ifstream learn_file(p, ios::binary);
+						size_t tr = 0;
+
+						/*
+						while (tr < fh.fsize){
+							learn_file.read(chunk, 1024);
+							tr += min(1024, (int)learn_file.gcount());
+							write(user1, asio::buffer(chunk, min(1024, (int)learn_file.gcount())));
+						}
+						*/
+
 						true_path = true;
 					}
 					else{
